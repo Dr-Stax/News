36a1944611665a05c0c7d744c8eb2f2a75826ba0
Steve Solomon <steveskyler700@gmail.com>
2025-08-01 10:15:47 -0700

    add Render URL to ALLOWED_HOSTS

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=False)
 
 #ALLOWED_HOSTS = ['news-ghw2.onrender.com'] 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ['news-u73d.onrender.com']
 #config('ALLOWED_HOSTS', default='').split(',')
 
 
