c15593c7d3206c117e7e03ac05854c2f80e33ec8
Steve Solomon <steveskyler700@gmail.com>
2025-08-01 09:12:55 -0700

    initial commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -35,8 +35,8 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 
 #DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = Env.bool("DEBUG", default=False)
-
+#DEBUG = Env.bool("DEBUG", default=False)
+DEBUG = env.bool("DEBUG", default=False)
 
 #ALLOWED_HOSTS = ['news-ghw2.onrender.com'] 
 ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
